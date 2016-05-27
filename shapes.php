a5261e9e68d1275106bf92523c9484bcf97d9d63
Tom <abaotom14@gmail.com>
2016-05-27 21:02:01 +0800

    started addr book

--- shapes.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* set out document type to text/javascript instead of text/html */
-header("Content-type: text/javascript");
-
-$ini = parse_ini_file("config.ini", true, INI_SCANNER_RAW);
-// $iniOpt = 'options';
-// var_dump($ini);
-
-echo json_encode($ini);
